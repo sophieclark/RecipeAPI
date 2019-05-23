086d0dfe3da3be0cd126d699d5885e653059da01
Sophie Clark <sophie.clark28@live.co.uk>
2019-05-23 14:53:12 +0100

    conform to reflection decodable

--- Sources/App/Models/Ingredient.swift
+++ Sources/App/Models/Ingredient.swift
@@ -1,23 +1,29 @@
 import Vapor
 import FluentPostgreSQL
 
+enum MeasurementType: String, Codable {
+  case grams
+  case kilograms
+  case litres
+  case mililetres
+  case centiletres
+  case ounce
+  case pound
+  case quantity
+}
+
+extension MeasurementType: ReflectionDecodable {
+  static func reflectDecoded() throws -> (MeasurementType, MeasurementType) {
+    return (.grams, .kilograms)
+  }
+}
+
 struct Ingredient: Codable {
   var id: Int?
   var recipeID: Recipe.ID
   var amount: Float
   var name: String
   var measurementType: MeasurementType
-  
-  enum MeasurementType: String, Codable {
-    case grams
-    case kilograms
-    case litres
-    case mililetres
-    case centiletres
-    case ounce
-    case pound
-    case quantity
-  }
 }
 
 extension Ingredient: PostgreSQLModel {}
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -13,36 +13,15 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
   services.register(middlewares)
 
   var databases = DatabasesConfig()
-  let databaseConfig: PostgreSQLDatabaseConfig
-  if let url = Environment.get("DATABASE_URL") {
-    databaseConfig = PostgreSQLDatabaseConfig(url: url)!
-  } else if let url = Environment.get("DB_POSTGRESQL") {
-    databaseConfig = PostgreSQLDatabaseConfig(url: url)!
-  } else {
-    let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
-    let username = Environment.get("DATABASE_USER") ?? "vapor"
-    let password = Environment.get("DATABASE_PASSWORD") ?? "password"
-    let databaseName: String
-    let databasePort: Int
-    if (env == .testing) {
-      databaseName = "vapor-test"
-      if let testPort = Environment.get("DATABASE_PORT") {
-        databasePort = Int(testPort) ?? 5433
-      } else {
-        databasePort = 5433
-      }
-    } else {
-      databaseName = Environment.get("DATABASE_DB") ?? "vapor"
-      databasePort = 5432
-    }
-    
-    databaseConfig = PostgreSQLDatabaseConfig(
-      hostname: hostname,
-      port: databasePort,
-      username: username,
-      database: databaseName,
-      password: password)
-  }
+  let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
+  let username = Environment.get("DATABASE_USER") ?? "vapor"
+  let databaseName = Environment.get("DATABASE_DB") ?? "vapor"
+  let password = Environment.get("DATABASE_PASSWORD") ?? "password"
+  let databaseConfig = PostgreSQLDatabaseConfig(
+    hostname: hostname,
+    username: username,
+    database: databaseName,
+    password: password)
   
   let database = PostgreSQLDatabase(config: databaseConfig)
   databases.add(database: database, as: .psql)
